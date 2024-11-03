b402f6a2ab7c0299acd0ab201648d31ce85e211e
avytr <tr.anhvy.work@gmail.com>
2024-11-04 00:31:21 +0700

    Update react app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import HomePage from './components/HomePage';
+import HomePage from './pages/HomePage/HomePage.jsx';
 
 function App() {  
   return (  
---
+++ front-end/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {  
+    return (  
+      <div>  
+        <h1>This is Homepage!</h1>  
+      </div>  
+    );  
+  };  
+  
+export default HomePage; 
