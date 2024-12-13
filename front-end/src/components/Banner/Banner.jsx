2c46cca78382a302060ca0e09f5378a3ad8e613d
avytran <tr.anhvy.work@gmail.com>
2024-12-13 21:11:31 +0700

    feat(US077): rename class name of banner to follow standard

--- front-end/src/components/Banner/Banner.jsx
+++ front-end/src/components/Banner/Banner.jsx
@@ -8,13 +8,13 @@ export const Banner = () => {
     return (  
         <div className="banner-section">  
             <div className="banner-list">  
-                <div className="banner-list__previous"></div>
+                <div className="banner-list-previous"></div>
                 {  
                     banners.map((banner, index) =>   
                         <img key={index} id={`banner-${index}`} className={`banner`} src={require(`../../mocks${banner.image}`)} alt={banner.title} />  
                     )  
                 }  
-                <div className="banner-list__next"></div>
+                <div className="banner-list-next"></div>
             </div>  
         </div>
     );  
