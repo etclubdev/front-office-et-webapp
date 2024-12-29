da23ec3cf4c24d6877d00f8166036457fd877d75
lequan <lequan2002.ql@gmail.com>
2024-12-29 21:39:32 +0700

    feat: adjust route and container css

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,24 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import { HomePage } from './pages/HomePage'; 
+
+// import { HomePage } from './pages/HomePage';
+import  TetCountDownTimerPage  from './pages/EventPages/TetCountDownTimerPage/TetCountDownTimerPage';
+
+import { createStar } from './utils/createStarUtil';
+
+import './App.css';
 
 function App() {  
+  useEffect(() => {  
+    for (let i = 0; i < 100; i++) {  
+        createStar('root-container');  
+    }  
+  }, []); 
   return (  
     <Router>  
-      <div>  
+      <div className="root-container">  
         <Routes>  
-          <Route path="/" element={<HomePage />} /> 
+          <Route path="/" element={<TetCountDownTimerPage />} /> 
         </Routes>  
       </div>  
     </Router>  
