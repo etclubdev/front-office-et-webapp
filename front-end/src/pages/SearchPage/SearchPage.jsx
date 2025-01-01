8ec4857efe1fa880e5cac509afce597d16a7e678
avytran <tr.anhvy.work@gmail.com>
2025-01-01 15:32:27 +0700

    feat(US185): add search page

--- front-end/src/pages/SearchPage/SearchPage.jsx
+++ front-end/src/pages/SearchPage/SearchPage.jsx
@@ -1,7 +1,27 @@
+import './SearchPage.css';
+import { SearchSuggestions } from '../../components/SearchSuggestions/SearchSuggestions';
+import { useState } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
 export const SearchPage = () => {
+
+    const [target, setTarget] = useState("");
+
     return (
-        <div className="">
-            
+        <div className="search-section">
+            <div className="search-container">
+                <div className="search-close">
+                    <a href="/"><FontAwesomeIcon className='close-btn' color='#fff' icon={faTimes} /></a>
+                </div>
+                <div className="search-input">
+                    <div className="vertical-bar"></div>
+                    <input placeholder='Nhập từ khóa tìm kiếm' value={target} type="text" onChange={(e) => setTarget(e.target.value)}/>
+                </div>
+                <div className="horizontal-bar"></div>
+                <SearchSuggestions target={target}/>
+            </div>
         </div>
     )
 }
