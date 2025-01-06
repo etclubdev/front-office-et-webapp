6d243ec7ef704832c570880652a6f51af9088765
avytran <tr.anhvy.work@gmail.com>
2025-01-06 17:33:12 +0700

    feat(US127): create HR structure page and set up route

---
+++ front-end/src/pages/HRStructurePage/HRStructurePage.jsx
@@ -0,0 +1,44 @@
+import './HRStructurePage.css';
+
+import { HRStructure } from '../../mocks/data';
+import HRStructureTree from '../../mocks/images/hr-structure/hr-structure-tree.png'
+
+const hrData = HRStructure.hrStructure;
+
+console.log(HRStructureTree);
+
+
+export const HRStructurePage = () => {
+    return (
+        <div className="hr-structure-page">
+            <div className="hr-structure-tree">
+                {
+                    hrData.map((department, index) => {
+                        if (index % 2 === 0)
+                            return (
+                                <div className="hr-structure-branch">
+                                    <img src={require(`../../mocks${department.image}`)} alt="" className="hr-structure-img" />
+                                    <div className="hr-structure-content">
+                                        <p className="hr-structure-name">{department.name}</p>
+                                        <p className="hr-structure-desc">{department.desc}</p>
+                                    </div>
+                                </div>
+                            )
+                        else {
+                            return (
+                                <div className="hr-structure-branch">
+                                    <div className="hr-structure-content">
+                                        <p className="hr-structure-name">{department.name}</p>
+                                        <p className="hr-structure-desc">{department.desc}</p>
+                                    </div>
+                                    <img src={require(`../../mocks${department.image}`)} alt="" className="hr-structure-img" />
+                                </div>
+                            )
+                        }
+                    })
+                }
+                <img className='hr-tree' src={HRStructureTree} alt="" />
+            </div>
+        </div>
+    )
+}
