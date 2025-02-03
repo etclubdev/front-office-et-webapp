a8e8ffae16d8de449be5cac2ba40e1a84fbf9539
avytran <tr.anhvy.work@gmail.com>
2025-02-03 22:51:10 +0700

    feat(US065): add icon assets

---
+++ front-end/src/assets/icons/index.js
@@ -0,0 +1,11 @@
+import shieldIcon from './shield.svg';
+import usersIcon from './users.svg';
+import fileIcon from './file-text.svg';
+import zapIcon from './zap.svg';
+import gridIcon from './grid.svg';
+import smileIcon from './smile.svg';
+import phoneIcon from './phone-outgoing.svg';
+import sendIcon from './send.svg'
+
+
+export { shieldIcon, usersIcon, fileIcon, zapIcon, gridIcon, smileIcon, phoneIcon, sendIcon };
