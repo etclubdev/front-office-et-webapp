e84a86e69fcb7fef6836476fb9a87071f72d6235
avytran <tr.anhvy.work@gmail.com>
2025-02-03 22:52:42 +0700

    feat(US065): set up route for page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // import { HomePage } from './pages/HomePage';
 import  TetCountDownTimerPage  from './pages/EventPages/TetCountDownTimerPage/TetCountDownTimerPage';
+import PersonalProfilePage from './pages/PersonalProfilePage';
 
 import { createStar } from './utils/createStarUtil';
 
@@ -19,6 +20,7 @@ function App() {
       <div className="root-container">  
         <Routes>  
           <Route path="/" element={<TetCountDownTimerPage />} /> 
+          <Route path="/personal-profile/*" element={<PersonalProfilePage />} /> 
         </Routes>  
       </div>  
     </Router>  
