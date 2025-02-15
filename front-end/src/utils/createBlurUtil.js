2e3e48ab498e588b14e2afdbf5e8fb96578017da
avytran <tr.anhvy.work@gmail.com>
2025-02-15 22:38:26 +0700

    feat: add util for creating  blur circles

---
+++ front-end/src/utils/createBlurUtil.js
@@ -0,0 +1,26 @@
+const createBlur = (className) => {
+    const gradients = [
+        "linear-gradient(90deg, rgba(128, 232, 241, 0.80) 0%, rgba(79, 170, 194, 0.80) 50%, #0D3DA4 100%)",
+        "linear-gradient(90deg, #D1FDFF 0%, #FDDB92 99.27%)",
+    ];
+
+    const parentElement = document.querySelector("." + className);
+    if (!parentElement) return [];
+
+    const pageHeight = parentElement.scrollHeight;
+    
+    const baseCount = 5;
+    const extraCount = Math.floor(pageHeight / 1000) * 5;
+    const count = baseCount + extraCount;
+
+    return Array.from({ length: count }).map(() => ({
+        size: Math.random() * 200 + 50, 
+        blur: Math.random() * 60 + 5,  
+        opacity: Math.random() * 0.4 + 0.2,
+        top: Math.random() * pageHeight + "px",
+        left: Math.random() * 120 - 20 + "%",
+        background: gradients[Math.floor(Math.random() * gradients.length)], // Chọn màu ngẫu nhiên
+    }));
+};
+
+export { createBlur };
