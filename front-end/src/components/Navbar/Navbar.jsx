23e4d6282c3dbedeb834ccb059f88960b744e363
lequan <lequan2002.ql@gmail.com>
2025-03-02 13:25:41 +0700

    fix: add mising dependencies

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState, useEffect } from 'react';
 import { horizontalLogo } from '../../assets/images/logos';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';  
