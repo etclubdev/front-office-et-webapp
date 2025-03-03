f0bea2e1935b4aaf4c5763cc451bf2b0443c6402
ArthurHoang15 <147850061+ArthurHoang15@users.noreply.github.com>
2025-03-03 22:41:03 +0700

    Feature/Add assets resource
    * chore(US180): add resources for homepage screen v2.0
    
    * chore(US180): adjust file content
    
    * chore(US180): replace unnecessary files

---
+++ front-end/src/mocks/data/index.js
@@ -0,0 +1,10 @@
+import characteristicsData from './characteristics.json';
+import bannerData from './banners.json';
+import bioData from './bio.json';
+import departmentsData from './departments.json';
+import partnersData from './partners.json';
+import statisticsData from './statistics.json';
+import companionsData from './companions.json';
+import FAQs from './FAQ.json';
+
+export { characteristicsData, bannerData, bioData, departmentsData, partnersData, statisticsData, companionsData, FAQs }
