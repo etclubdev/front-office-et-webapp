b7f34ccc9ebf22372b16118b466da40b2f9cbc84
avytran <tr.anhvy.work@gmail.com>
2025-04-14 01:05:38 +0700

    style(US97): responsive page

--- front-end/src/components/ETNewsSlider/ETNewsSlider.jsx
+++ front-end/src/components/ETNewsSlider/ETNewsSlider.jsx
@@ -21,7 +21,7 @@ const settings = {
     variableWidth: false, 
     responsive: [
         { breakpoint: 1200, settings: { slidesToShow: 3, slidesToScroll: 1 } },
-        { breakpoint: 768, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+        { breakpoint: 768, settings: { slidesToShow: 3, slidesToScroll: 1 } },
         { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1,  arrows: false} }
     ]
 };
