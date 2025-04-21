f8a9db00739b68243075895086489cc260fbd408
Anh Vy Tran <tr.anhvy.work@gmail.com>
2025-04-21 17:04:02 +0700

    Feature/US186 Develop FAQs Screen (#25)
    * feat(US186): install dependencies
    
    * feat(US186): add Montserrat fonts
    
    * feat(US186): add mocks data
    
    * feat(US186): add FAQs page, component and set up route
    
    * feat(US186): add topic question component
    
    * feat(US186): update mocks data and FAQs page
    
    * feat(US186): install dependencies
    
    * fix(US186): modify setState condition
    
    * feat(US186): add api service and improve components structure
    
    * fix(US186): correct newline rendering in FAQ answers

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { HomePage } from './pages/HomePage';
 import  TetCountDownTimerPage  from './pages/EventPages/TetCountDownTimerPage/TetCountDownTimerPage';
+import { FAQsPage } from './pages/FAQsPage';
 import { IntroductionPage } from './pages/IntroductionPage';
 import { HRStructurePage } from './pages/HRStructurePage/HRStructurePage';
 
@@ -17,6 +18,7 @@ function App() {
         <DynamicStars parentClassName="root-container"/>
         <DynamicBlur parentClassName="root-container"/>
         <Routes>  
+          <Route path="/faqs" element={<FAQsPage />} /> 
           <Route path="/introduction" element={<IntroductionPage />} /> 
           <Route path="/hr-structure" element={<HRStructurePage />} /> 
           <Route path="/" element={<HomePage />} /> 
---
+++ front-end/src/components/PageTitle/PageTitle.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './PageTitle.css'
+
+export const PageTitle = ({ children }) => {
+  return (
+    <p className="page-title">{children}</p>
+  )
+}
