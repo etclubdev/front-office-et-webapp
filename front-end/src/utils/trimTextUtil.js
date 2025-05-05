8c0d44b9207aae3ef476376755b84b7710f12f0f
avytran <tr.anhvy.work@gmail.com>
2025-05-05 14:01:36 +0700

    chore: responsive about et pages

--- front-end/src/utils/trimTextUtil.js
+++ front-end/src/utils/trimTextUtil.js
@@ -1,5 +1,4 @@
-export function trimText(text) {
-    const maxLength = 160;
+export function trimText(text, maxLength) {
     if (text.length > maxLength) {
         return text.slice(0, maxLength - 3) + '...';
     }
