1f331940530d6a8dedbe5306f812347648805a9f
lequan <lequan2002.ql@gmail.com>
2025-05-17 01:34:12 +0700

    feat: add Error component

---
+++ front-end/src/components/Error/Error.jsx
@@ -0,0 +1,12 @@
+import './Error.css'
+
+export const Error = () => {
+    return (
+        <div className='error-container'>
+            <img src={require(`../../mocks/images/error/error.png`)} alt="null" className="error-img" />
+            <p className="nav-item nav-item-content error-content">Something went wrong</p>
+            <p className="error-content-detail">We had a problem loading your content.</p>
+            <p className="error-content-detail">Try refreshing the page.</p>
+        </div>
+    )
+}
