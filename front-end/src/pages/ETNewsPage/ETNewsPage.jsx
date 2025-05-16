5f265cc2bddaacb8cef3c4d19f6953af600ccd45
lequan <lequan2002.ql@gmail.com>
2025-05-17 02:05:10 +0700

    feat: uplift et news page

--- front-end/src/pages/ETNewsPage/ETNewsPage.jsx
+++ front-end/src/pages/ETNewsPage/ETNewsPage.jsx
@@ -1,12 +1,13 @@
 import './ETNewsPage.css';
 import { Navbar } from '../../components/Navbar';
-import { Footer } from '../../components/Footer';
 import { HorizontalNews } from '../../components/HorizontalNews';
 import { VerticalNews } from '../../components/VerticalNews';
 import { PostSlider } from '../../components/PostSlider';
-
 import { getAllNews } from '../../api/etNews.service';
 import { useEffect, useState } from 'react';
+import { CustomBreadcrumbs } from '../../components/CustomBreadcrumbs'
+import { CircularLoading } from '../../components/CircularLoading';
+import { DynamicBlur } from '../../components/DynamicBlur';
 
 export const ETNewsPage = () => {
     const [news, setNews] = useState([]);
@@ -17,11 +18,33 @@ export const ETNewsPage = () => {
         }
         fetchData();
     }, [])
-    
+
+    if (!news || news.length === 0) {
+        return (
+            <div className="loading...">
+                <Navbar />
+                <CircularLoading />
+            </div>
+
+        );
+    }
+
+    const breadcrumbsData = [
+        {
+            href: "/",
+            title: "Trang chủ"
+        },
+        {
+            href: "#",
+            title: "Bản tin ET"
+        }
+    ]
 
     return (
         <div className="etnews-page">
             <Navbar />
+            <DynamicBlur parentClassName="root-container" />
+            <CustomBreadcrumbs data={breadcrumbsData} style={{ width: "70%" }}></CustomBreadcrumbs>
             <div className="et-news-section">
                 <div className="et-news-highlight">
                     <div className="highlight-title">ET NEWS</div>
@@ -31,33 +54,30 @@ export const ETNewsPage = () => {
                         </div>
                         <div id="highlight-news-2">
                             {
-                                news?.latestNews?.map((item, index) => {
-                                    if (index !== 0)
-                                        return (
-                                            <HorizontalNews isETNews key={'highlight-news-' + index} news={item} />
-                                        )
-                                })
+                                news?.latestNews
+                                    ?.filter((_, index) => index !== 0)
+                                    .map((item, index) => (
+                                        <HorizontalNews isETNews key={'highlight-news-' + index} news={item} />
+                                    ))
                             }
                         </div>
                     </div>
                 </div>
                 <div className="et-news-categories">
                     {
-                        Object.entries(news?.groupedNews || {}).map(([category, newsList], index) => {
-                            if (newsList?.length > 0){
-                                return (
-                                    <PostSlider 
-                                        key={`news-list` + index}
-                                        newsList={newsList}
-                                        title={category}
-                                        categoryid={`news-list` + index}
-                                        isETNews
-                                    />
-                                )
-                            }
-                            
-                        })
+                        Object.entries(news?.groupedNews || {})
+                            .filter(([_, newsList]) => newsList?.length > 0)
+                            .map(([category, newsList], index) => (
+                                <PostSlider
+                                    key={`news-list-${index}`}
+                                    newsList={newsList}
+                                    title={category}
+                                    categoryid={`news-list-${index}`}
+                                    isETNews
+                                />
+                            ))
                     }
+
                 </div>
             </div>
         </div>
