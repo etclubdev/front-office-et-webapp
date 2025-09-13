099b22d0c2c5089ccb531c2481e1c616f10883b2
avytran <tr.anhvy.work@gmail.com>
2025-09-14 02:17:58 +0700

    chore: posts css enhancement

--- front-end/src/components/PostDetails/PostDetails.jsx
+++ front-end/src/components/PostDetails/PostDetails.jsx
@@ -12,22 +12,23 @@ export const PostDetails = ({ details, thumbnailShowed = false }) => {
     return (
         <div className="details-post">
             <div className="details-container">
-                <Heading level={1} className="details-title">{details.title}</Heading>
-                <div className="details-info">
-                    <div className="details-info-item" id="details-time">
-                        <FontAwesomeIcon icon={faClock} />
-                        {createdOn}
-                    </div>
-                    <div className="details-info-item">
-                        {details.blog_author ?
-                            <>
-                                <FontAwesomeIcon icon={faUserTie} />
-                                {details.blog_author}
-                            </>
-                            : <></>}
+                <div className="details-content-header">
+                    <Heading level={1} className="details-title">{details.title}</Heading>
+                    <div className="details-info">
+                        <div className="details-info-item" id="details-time">
+                            <FontAwesomeIcon icon={faClock} />
+                            {createdOn}
+                        </div>
+                        <div className="details-info-item">
+                            {details.blog_author ?
+                                <>
+                                    <FontAwesomeIcon icon={faUserTie} />
+                                    {details.blog_author}
+                                </>
+                                : <></>}
+                        </div>
                     </div>
                 </div>
-
                 <div className="img-container" style={{ display: "flex", justifyContent: "center" }}>
                     {thumbnailShowed && <img src={details.thumbnail_image_url} alt="" />}
                 </div>
@@ -36,7 +37,7 @@ export const PostDetails = ({ details, thumbnailShowed = false }) => {
                     {details.source &&
                         <p className="details-references">
                             <strong>Nguồn: </strong>
-                            {details.source.includes("http") ? 
+                            {details.source.includes("http") ?
                                 <a href={details.source}>{details.source}</a> :
                                 details.source}
                         </p>}
